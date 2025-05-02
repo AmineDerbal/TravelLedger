e44e523bebd70060b313c602ba98f43336575b19
Amine derbal <aminedoran31@gmail.com>
2025-05-02 19:55:16 +0100

    Config pinia plugin

--- resources/js/plugins/2.pinia.js
+++ resources/js/plugins/2.pinia.js
@@ -1,6 +1,7 @@
-import { createPinia } from 'pinia'
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
-export const store = createPinia()
+export const store = createPinia().use(piniaPluginPersistedstate);
 export default function (app) {
-  app.use(store)
+  app.use(store);
 }
