f3ae2b565b2c910f3276073964c089ce01e71589
AmineDerbal <aminedoran31@gmail.com>
2025-05-04 10:19:44 +0100

    Set api config

--- resources/js/utils/api.js
+++ resources/js/utils/api.js
@@ -1,10 +1,13 @@
-import { ofetch } from 'ofetch'
+import { ofetch } from 'ofetch';
+import axios from '@/utils/axios';
 
 export const $api = ofetch.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
   async onRequest({ options }) {
-    const accessToken = useCookie('accessToken').value
+    const accessToken = useCookie('accessToken').value;
     if (accessToken)
-      options.headers.append('Authorization', `Bearer ${accessToken}`)
+      options.headers.append('Authorization', `Bearer ${accessToken}`);
   },
-})
+});
+
+export const apiRequest = async (options) => {};
