fc454c392ba67f5311a2c7ad3bb661aed046aa3a
AmineDerbal <aminedoran31@gmail.com>
2025-05-06 14:26:24 +0100

    Update login method

--- resources/js/store/user.js
+++ resources/js/store/user.js
@@ -1,14 +1,21 @@
 import { defineStore } from 'pinia';
+import { apiCall, apiAction } from '@/utils/api';
 
-export default userStore = defineStore('user', {
+export default defineStore('user', {
   state: () => ({
     user: null,
+    isAuthenticated: false,
     hasError: false,
     errors: {},
   }),
   persist: true,
 
   actions: {
-    async login(data) {},
+    async login(data) {
+      return await apiAction(() => apiCall('auth/login', 'POST', data), this);
+    },
+    setIsAuthenticated(value) {
+      this.isAuthenticated = value;
+    },
   },
 });
