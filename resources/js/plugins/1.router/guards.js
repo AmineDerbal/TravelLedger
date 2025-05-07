aac290ab7ec70bb74f3cc8ab89ccaa32e30f54cb
AmineDerbal <aminedoran31@gmail.com>
2025-05-07 12:44:18 +0100

    Add guards for login

--- resources/js/plugins/1.router/guards.js
+++ resources/js/plugins/1.router/guards.js
@@ -2,17 +2,28 @@ import useUserStore from '@/store/user';
 export const setupGuards = (router) => {
   // 👉 router.beforeEach
   // Docs: https://router.vuejs.org/guide/advanced/navigation-guards.html#global-before-guards
-  router.beforeEach((to) => {
+  router.beforeEach((to, from) => {
     /*
      * If it's a public route, continue navigation. This kind of pages are allowed to visited by login & non-login users. Basically, without any restrictions.
      * Examples of public routes are, 404, under maintenance, etc.
      */
-    if (to.meta.public) return;
 
-    // If user is not logged in, redirect to login page.
     const userStore = useUserStore();
     const isLoggedIn = userStore.isAuthenticated;
 
+    // ⛔ Redirect authenticated users away from guest-only routes (e.g., login, register)
+    if (to.meta.loginRouteGuard && isLoggedIn) {
+      // If coming from a route, redirect back to it; else fallback to '/'
+
+      return from.name ? from.fullPath : '/';
+    }
+
+    if (to.meta.public) return;
+
+    // If user is not logged in, redirect to login page.
+
     if (!isLoggedIn && to.meta.requiresAuth) return '/login';
+
+    return;
   });
 };
