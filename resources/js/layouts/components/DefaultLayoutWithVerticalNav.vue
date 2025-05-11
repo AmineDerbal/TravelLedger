b5bbb53f9181884b0f9c142d6ad227016b56f554
AmineDerbal <aminedoran31@gmail.com>
2025-05-11 15:59:28 +0100

    Add ledger box to header

--- resources/js/layouts/components/DefaultLayoutWithVerticalNav.vue
+++ resources/js/layouts/components/DefaultLayoutWithVerticalNav.vue
@@ -1,15 +1,31 @@
 <script setup>
-import navItems from '@/navigation/vertical'
-import { themeConfig } from '@themeConfig'
+import navItems from '@/navigation/vertical';
+import { themeConfig } from '@themeConfig';
 
 // Components
-import Footer from '@/layouts/components/Footer.vue'
-import NavbarThemeSwitcher from '@/layouts/components/NavbarThemeSwitcher.vue'
-import UserProfile from '@/layouts/components/UserProfile.vue'
-import NavBarI18n from '@core/components/I18n.vue'
+import Footer from '@/layouts/components/Footer.vue';
+import NavbarThemeSwitcher from '@/layouts/components/NavbarThemeSwitcher.vue';
+import UserProfile from '@/layouts/components/UserProfile.vue';
+import NavBarI18n from '@core/components/I18n.vue';
 
 // @layouts plugin
-import { VerticalNavLayout } from '@layouts'
+import { VerticalNavLayout } from '@layouts';
+
+// stores
+import useLedgerStore from '@/store/ledger';
+
+const ledgerItem = {
+  color: 'success',
+  icon: 'tabler-currency-dollar',
+};
+
+const ledgerStore = useLedgerStore();
+const ledger = computed(() => ledgerStore.ledger);
+onBeforeMount(async () => {
+  if (!ledger.value) {
+    await ledgerStore.getFirstLedger();
+  }
+});
 </script>
 
 <template>
@@ -33,9 +49,37 @@ import { VerticalNavLayout } from '@layouts'
         <VSpacer />
 
         <NavBarI18n
-          v-if="themeConfig.app.i18n.enable && themeConfig.app.i18n.langConfig?.length"
+          v-if="
+            themeConfig.app.i18n.enable &&
+            themeConfig.app.i18n.langConfig?.length
+          "
           :languages="themeConfig.app.i18n.langConfig"
         />
+        <VSpacer />
+        <VCardText>
+          <VRow>
+            <VCol
+              cols="6"
+              md="3"
+            >
+              <div class="d-flex gap-x-4 align-center">
+                <VAvatar
+                  :color="ledgerItem.color"
+                  variant="tonal"
+                  size="40"
+                  rounded
+                >
+                  <VIcon :icon="ledgerItem.icon" />
+                </VAvatar>
+                <div class="d-flex flex-column">
+                  <h5 class="text-h5 d-flex">{{ ledger.name }}</h5>
+                  <div class="text-body-2">{{ ledger.amount }}&nbsp;DZD</div>
+                </div>
+              </div>
+            </VCol>
+          </VRow>
+        </VCardText>
+
         <UserProfile />
       </div>
     </template>
