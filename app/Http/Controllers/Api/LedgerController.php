44b4e630f26de26fc570731120e94b336f348d50
AmineDerbal <aminedoran31@gmail.com>
2025-05-11 09:45:26 +0100

    Add ledger controller

---
+++ app/Http/Controllers/Api/LedgerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Ledger;
+
+class LedgerController extends Controller
+{
+   public function getFirstLedger()
+   {
+       $ledger = Ledger::first();
+       return response()->json($ledger);
+   }
+}
