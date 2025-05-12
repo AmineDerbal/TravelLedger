398907dc977595016318d66d594dde9c2e687840
AmineDerbal <aminedoran31@gmail.com>
2025-05-12 13:46:35 +0100

    Update transaction page

--- resources/js/pages/transaction/index.vue
+++ resources/js/pages/transaction/index.vue
@@ -2,10 +2,26 @@
 
 <script setup>
 import { definePage } from 'vue-router/auto';
+import { useTransactionStore } from '@/store/transaction';
+import { computed } from 'vue';
 
 definePage({
   meta: {
     requiresAuth: true,
   },
 });
+
+const transactionStore = useTransactionStore();
+
+const transactions = computed(() => {
+  return transactionStore.transactions;
+});
 </script>
+
+<template>
+  <VBtn variant="tonal">Add Transaction</VBtn>
+  <div v-if="transactions.length < 1">
+    <h2>There are no transactions</h2>
+  </div>
+  <div v-else></div>
+</template>
