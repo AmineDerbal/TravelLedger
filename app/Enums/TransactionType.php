892e249a391e3f4198fde65af7d312843678fdc0
Amine derbal <aminedoran31@gmail.com>
2025-05-16 17:10:01 +0100

    Add values method

--- app/Enums/TransactionType.php
+++ app/Enums/TransactionType.php
@@ -4,15 +4,28 @@ namespace App\Enums;
 
 enum TransactionType: int
 {
-case Credit = 1;
-case Debit = 2;
+    case Credit = 1;
+    case Debit = 2;
 
 
-public function label(): string
-{
-return match ($this) {
-  self::Credit => 'Credit',
-  self::Debit => 'Debit',
-};
-}
+    public function label(): string
+    {
+        return match ($this) {
+            self::Credit => 'Credit',
+            self::Debit => 'Debit',
+        };
+    }
+    public static function values(): array
+    {
+        return [
+          [
+            'value' => self::Credit,
+            'label' => self::Credit->label(),
+          ],
+          [
+            'value' => self::Debit,
+            'label' => self::Debit->label(),
+          ],
+        ];
+    }
 }
