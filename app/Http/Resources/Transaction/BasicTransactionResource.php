8e960524709af8714e2b5f612a97a8a94cc2ded6
AmineDerbal <aminedoran31@gmail.com>
2025-05-19 15:05:44 +0100

    Add data format for transactions

---
+++ app/Http/Resources/Transaction/BasicTransactionResource.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Resources\Transaction;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+use App\Enums\TransactionType;
+use App\Enums\TransactionCategory;
+
+class BasicTransactionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user' => [
+                'id' => $this->user->id,
+                'name' => $this->user->name,],
+            'ledger' => [
+                'id' => $this->ledger->id,
+                'name' => $this->ledger->name,],
+            'type' => [
+                'value' => $this->type,
+                'label' => TransactionType::labelFromValue($this->type)
+            ],
+            'category' => [
+                'value' => $this->category,
+                'label' => TransactionCategory::labelFromValue($this->category)
+            ],
+            'amount' => $this->amount,
+            'date' => $this->date,
+            'description' => $this->description
+        ];
+    }
+}
