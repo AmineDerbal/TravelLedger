e03ab68ebe8ecbdb4b48271b863aa9a6e4530f32
AmineDerbal <aminedoran31@gmail.com>
2025-05-20 10:42:18 +0100

    Add update method

--- app/Models/Ledger.php
+++ app/Models/Ledger.php
@@ -5,7 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Enums\TransactionType;
+
 class Ledger extends Model
 {
     use HasFactory;
+
+    public function updateAmount($amount, $type) {
+        if(TransactionType::labelFromValue($type) == 'Credit') {
+            $this->amount += $amount;
+        } else {
+            $this->amount -= $amount;
+        }
+        $this->save();
+    } 
 }
