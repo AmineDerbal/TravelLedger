fb5b535a69b326b0dd023eb732bb1d6fcfdc9b60
AmineDerbal <aminedoran31@gmail.com>
2025-05-20 10:42:40 +0100

    Use service for store

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -4,16 +4,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Transaction;
-use App\Models\Ledger;
 use App\Http\Requests\Transaction\storeTransactionRequest;
 use App\Http\Requests\Transaction\getTransactionsByDateRangeRequest;    
 use App\Http\Resources\Transaction\BasicTransactionResource;
-use App\Enums\TransactionType;
+use App\Services\TransactionService;
 
-use Illuminate\Http\Request;
+
 
 class TransactionController extends Controller
 {
+    protected $transactionService;
+
+    public function __construct(TransactionService $transactionService) {
+        $this->transactionService = $transactionService;
+    }
     public function getTransactionsByDateRange(getTransactionsByDateRangeRequest $request) {
         $data = $request->validated();
         $transactions = Transaction::where('ledger_id', $data['ledger_id'])->whereBetween('date', [$data['start_date'], $data['end_date']])->get();
@@ -23,23 +27,8 @@ class TransactionController extends Controller
     public function store(storeTransactionRequest $request) {
         $data = $request->validated();
 
-        $transaction = Transaction::create($data);
-        if (!$transaction) {
-            return response()->json(['message' => 'Transaction creation failed'], 500);
-        }
+        return $this->transactionService->createTransaction($data);
 
-        $ledger = Ledger::find($data['ledger_id']);
-        if (!$ledger) {
-            $transaction->delete();
-            return response()->json(['message' => 'Ledger not found'], 404);
-        }
-            if(TransactionType::labelFromValue($data['type']) == 'Credit') {
-                $ledger->amount += $data['amount'];
-            } else {
-                $ledger->amount -= $data['amount'];
-            }
-            $ledger->save();
-            return response()->json(['message' => 'Transaction created successfully'], 201);
        
     }
 }
