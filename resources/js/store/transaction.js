82ffb844a000495d94af7a993238d52f5dc608bf
AmineDerbal <aminedoran31@gmail.com>
2025-05-26 12:43:47 +0100

    Add api call for excel download

--- resources/js/store/transaction.js
+++ resources/js/store/transaction.js
@@ -61,5 +61,28 @@ export default defineStore('transaction', {
         this,
       );
     },
+
+    async downloadExcelTransactions(data) {
+      return await apiAction(async () => {
+        const response = await apiCall(
+          'export-transactions',
+          'POST',
+          data,
+          'blob',
+        );
+        const blob = new Blob([response.data], {
+          type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        });
+
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'transactions.xlsx');
+        document.body.appendChild(link);
+        link.click();
+        URL.revokeObjectURL(link.href);
+        return response;
+      }, this);
+    },
   },
 });
