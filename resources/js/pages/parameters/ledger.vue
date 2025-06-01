2082649f39e235be451e4aa6d96e410cb97d5216
AmineDerbal <aminedoran31@gmail.com>
2025-06-01 09:43:52 +0100

    Add api call for ledgers index method

--- resources/js/pages/parameters/ledger.vue
+++ resources/js/pages/parameters/ledger.vue
@@ -1,9 +1,22 @@
 <template></template>
 
 <script setup>
+import useLedgerStore from '@/store/ledger';
 definePage({
   meta: {
     requiresAuth: true,
   },
+});
+
+const ledgerStore = useLedgerStore();
+const ledgers = computed(() => ledgerStore.ledgers);
+
+const headers = [
+  { title: 'Name', key: 'name' },
+  { title: 'Amount', key: 'amount' },
+];
+
+onBeforeMount(async () => {
+  await ledgerStore.getLedgers();
 });
 </script>
