ba21959db693f9b3051668c5ab8f57c0b3918c47
AmineDerbal <aminedoran31@gmail.com>
2025-06-04 09:36:00 +0100

    Add ledger category controller

---
+++ app/Http/Controllers/Api/LedgerCategoryController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\LedgerCategory;
+use App\Models\Ledger;
+use App\Enums\TransactionType;
+use App\Http\Requests\LedgerCategory\storeLedgerCategoryRequest;
+
+class LedgerCategoryController extends Controller
+{
+    public function index() {
+        $ledgerCategories = LedgerCategory::all();
+        return response()->json($ledgerCategories);
+    }
+
+    public function ledgerCategoriesOptions() {
+        $ledgerOptions = Ledger::select('id', 'name')->get();
+        $typeOptions = TransactionType::values();
+        return response()->json(['ledgerOptions' => $ledgerOptions, 'typeOptions' => $typeOptions]);
+    }
+
+    public function store(storeLedgerCategoryRequest $request) {
+        $data = $request->validated();        
+        $ledgerCategory = LedgerCategory::create($data);
+        if (!$ledgerCategory) {
+            return response()->json(['message' => 'Ledger category creation failed'], 500);
+        }
+        return response()->json(['message' => 'Ledger category created successfully'], 201);
+
+    }
+}
