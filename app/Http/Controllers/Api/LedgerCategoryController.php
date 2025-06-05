5bdb061cadcf92503b6b48d13515c64963fb1995
Amine derbal <aminedoran31@gmail.com>
2025-06-05 15:31:10 +0100

    Update data sent

--- app/Http/Controllers/Api/LedgerCategoryController.php
+++ app/Http/Controllers/Api/LedgerCategoryController.php
@@ -7,22 +7,26 @@ use App\Models\LedgerCategory;
 use App\Models\Ledger;
 use App\Enums\TransactionType;
 use App\Http\Requests\LedgerCategory\storeLedgerCategoryRequest;
+use App\Http\Resources\LedgerCategory\BasicLedgerCategoryResource;
 
 class LedgerCategoryController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $ledgerCategories = LedgerCategory::with('ledger')->get();
-        return response()->json($ledgerCategories);
+        return response()->json(BasicLedgerCategoryResource::collection($ledgerCategories));
     }
 
-    public function ledgerCategoriesOptions() {
+    public function ledgerCategoriesOptions()
+    {
         $ledgerOptions = Ledger::select('id', 'name')->get();
         $typeOptions = TransactionType::values();
         return response()->json(['ledgerOptions' => $ledgerOptions, 'typeOptions' => $typeOptions]);
     }
 
-    public function store(storeLedgerCategoryRequest $request) {
-        $data = $request->validated();        
+    public function store(storeLedgerCategoryRequest $request)
+    {
+        $data = $request->validated();
         $ledgerCategory = LedgerCategory::create($data);
         if (!$ledgerCategory) {
             return response()->json(['message' => 'Ledger category creation failed'], 500);
