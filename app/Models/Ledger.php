9ffafe9194a2a60dc63123a15390f899637c6fac
Amine derbal <aminedoran31@gmail.com>
2025-06-08 14:20:29 +0100

    Update model

--- app/Models/Ledger.php
+++ app/Models/Ledger.php
@@ -4,19 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 use App\Enums\TransactionType;
 
 class Ledger extends Model
 {
     use HasFactory;
 
-    public function updateAmount($amount, $type) {
-        if(TransactionType::labelFromValue($type) == 'Credit') {
+    protected $fillable = ['name','balance'];
+
+    public function updateAmount($amount, $type)
+    {
+        if (TransactionType::labelFromValue($type) == 'Credit') {
             $this->amount += $amount;
         } else {
             $this->amount -= $amount;
         }
         $this->save();
-    } 
+    }
+
+    public function categories()
+    {
+        return $this->hasMany(LedgerCategory::class);
+    }
+
 }
