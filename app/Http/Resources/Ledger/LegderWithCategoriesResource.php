495c7cdb1d345dc1a75093590eab9790823305ef
Amine derbal <aminedoran31@gmail.com>
2025-06-08 14:20:15 +0100

    Add ledger options resource

---
+++ app/Http/Resources/Ledger/LegderWithCategoriesResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Ledger;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\LedgerCategory\LedgerCategorySelectResource;
+
+class LegderWithCategoriesResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'categories' => LedgerCategorySelectResource::collection($this->categories)
+        ];
+    }
+}
