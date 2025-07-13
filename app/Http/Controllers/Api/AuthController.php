7a62a4f912393885c23f4b03eb707ae1e9b03d3a
AmineDerbal <aminedoran31@gmail.com>
2025-07-13 11:44:04 +0100

    Add role

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -70,6 +70,7 @@ class AuthController extends Controller
             'id' => $user->id,
             'name' => $user->name,
             'email' => $user->email,
+            'role' => $role,
 
         ];
 
