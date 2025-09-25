5bc926036b5b9034273d10ffc7cd680e1dfe96eb
Amine derbal <aminedoran31@gmail.com>
2025-09-25 19:28:05 +0100

    Add form

--- resources/js/components/users/UserDialog.vue
+++ resources/js/components/users/UserDialog.vue
@@ -73,6 +73,68 @@ const form = reactive({ ...props.formData });
               {{ errors.name[0] }}
             </div>
           </VCol>
+
+          <VCol cols="12">
+            <VTextField
+              v-model="form.email"
+              label="Email"
+            />
+            <div
+              class="mt-2 text-error"
+              v-if="errors.email"
+            >
+              {{ errors.email[0] }}
+            </div>
+          </VCol>
+
+          <VCol cols="12">
+            <VTextField
+              v-model="form.password"
+              label="Password"
+              type="password"
+            />
+            <div
+              class="mt-2 text-error"
+              v-if="errors.password"
+            >
+              {{ errors.password[0] }}
+            </div>
+          </VCol>
+
+          <VCol cols="12">
+            <VTextField
+              v-model="form.password_confirmation"
+              label="Confirm Password"
+              type="password"
+            />
+            <div
+              class="mt-2 text-error"
+              v-if="errors.password_confirmation"
+            >
+              {{ errors.password_confirmation[0] }}
+            </div>
+          </VCol>
+
+          <VCol cols="12">
+            <AppSelect
+              v-model="form.role"
+              :hint="form.role"
+              label="Role"
+              :items="roleOptions"
+              item-title="name"
+              item-value="id"
+              persistent-hint
+              return-object
+              single-line
+              placeholder="Select Role"
+            />
+            <div
+              class="mt-2 text-error"
+              v-if="errors.role_id"
+            >
+              {{ errors.role_id[0] }}
+            </div>
+          </VCol>
         </VRow>
       </VCardText>
     </VCard>
