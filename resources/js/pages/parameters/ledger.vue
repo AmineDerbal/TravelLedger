226d17bac7a53b213d5aab810a8dde29a827b3c3
AmineDerbal <aminedoran31@gmail.com>
2025-11-26 11:07:59 +0100

    Update ledger

--- resources/js/pages/parameters/ledger.vue
+++ resources/js/pages/parameters/ledger.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { useAbility } from '@/plugins/casl/composables/useAbility';
 import useLedgerStore from '@/store/ledgerStore';
 definePage({
   meta: {
@@ -9,7 +10,19 @@ definePage({
 });
 
 const ledgerStore = useLedgerStore();
+const ability = useAbility();
+
 const ledgers = computed(() => ledgerStore.ledgers);
+const errors = computed(() => ledgerStore.errors);
+
+const isDialogVisible = ref(false);
+const isEdit = ref(false);
+
+const defaultFormData = {
+  name: '',
+};
+
+const formData = ref({ ...defaultFormData });
 
 const headers = [
   { title: 'Name', key: 'name' },
@@ -23,7 +36,25 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-  <VCard>
+  <LedgerDialog v-model:isDialogVisible="isDialogVisible" />
+  <VCard title="Ledgers">
+    <VCardText>
+      <VRow class="align-end">
+        <VCol
+          cols="12"
+          md="4"
+        >
+          <VBtn
+            v-if="ability.can('manage', 'Ledger')"
+            variant="tonal"
+            color="primary"
+            @click="isDialogVisible = true"
+            >Add Ledger</VBtn
+          >
+        </VCol>
+      </VRow>
+    </VCardText>
+    <VDivider />
     <LedgerTable
       :ledgers="ledgers"
       :headers="headers"
