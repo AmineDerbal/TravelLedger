f55859e97a8df7169f3df0a198dc48eb472968ec
AmineDerbal <aminedoran31@gmail.com>
2025-11-26 10:54:33 +0100

    setup ledger dialog

---
+++ resources/js/components/ledgers/LedgerDialog.vue
@@ -0,0 +1,55 @@
+<script setup>
+const props = defineProps({
+  isDialogVisible: {
+    type: Boolean,
+    required: true,
+  },
+  formData: {
+    type: Object,
+    required: true,
+  },
+  errors: {
+    type: Object,
+    required: true,
+  },
+  dialogSubmitLoading: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+
+  isEdit: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+
+const emit = defineEmits([
+  'update:isDialogVisible',
+  'submit',
+  'closeEditDialog',
+]);
+
+const SetDialogVisible = (val) => {
+  emit('update:isDialogVisible', val);
+};
+
+const closeEditDialog = () => {
+  emit('closeEditDialog');
+};
+
+const closeDialog = () => {
+  props.isEdit ? closeEditDialog() : SetDialogVisible(false);
+};
+</script>
+
+<template>
+  <VDialog
+    v-model="isDialogVisible"
+    max-width="600"
+    @click:outside="closeDialog()"
+  >
+    <DialogCloseBtn @click="closeDialog()" />
+  </VDialog>
+</template>
